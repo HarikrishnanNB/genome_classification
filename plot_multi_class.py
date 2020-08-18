8ed42e75bfe07627be3db8ca8c88a2cef200f0a2
Harikrishnan NB <30375473+HarikrishnanNB@users.noreply.github.com>
2020-08-18 12:00:45 +0530

    updated plot files

--- plot_multi_class.py
+++ plot_multi_class.py
@@ -30,28 +30,29 @@ SAMPLES_PER_CLASS = np.load(RESULT_PATH+"/samples_per_class.npy")
 # Plotting F1-score vs. Samples per class
 plt.figure(figsize=(15,10))
 
-plt.plot(SAMPLES_PER_CLASS ,FSCORE_NEUROCHAOS[:,0], '-k', linewidth = 2.0,  linestyle='--', color ='k', marker='s', mfc='red', mec='green', ms=10, mew=4, label="Neurochaos-SVM")
-plt.plot(SAMPLES_PER_CLASS ,FSCORE_SVM[:,0], '--r', linewidth = 2.0, linestyle='solid', color ='r', marker='o',mfc='black', mec='green', ms=10, mew=4, label="SVM (linear)")
+plt.plot(SAMPLES_PER_CLASS ,FSCORE_NEUROCHAOS[:,0], linewidth = 2.0,  linestyle='solid', color ='r', marker='s', ms=10, label="Neurochaos-SVM")
+plt.plot(SAMPLES_PER_CLASS ,FSCORE_SVM[:,0], linewidth = 2.0, linestyle='--', color ='k', marker='o', ms=10, label="SVM (linear)")
 
 plt.xticks(SAMPLES_PER_CLASS,fontsize=22)
 plt.yticks(fontsize=22)
 plt.grid(True)
 plt.xlabel('Number of training samples per class', fontsize=22)
 plt.ylabel('Average F1-score', fontsize=22)
-plt.legend(loc="upper right", fontsize=22)
-plt.savefig(RESULT_PATH+ "/F1_low_training_sample_regime_multi_class_classification.jpg", format='jpg', dpi=200)
+plt.legend(loc="lower right", fontsize=22)
+plt.savefig(RESULT_PATH+ "/F1_low_training_sample_regime_multi_class_classification.jpg", format='jpg', dpi=300)
 plt.show()
 
 # Plotting Standard deviation of F1-score vs. smaples per class
 plt.figure(figsize=(15,10))
-plt.plot(SAMPLES_PER_CLASS ,STD_FSCORE_NEUROCHAOS[:,0], '-k', linewidth = 2.0,  linestyle='--', color ='k', marker='s', mfc='red', mec='green', ms=10, mew=4, label="Neurochaos-SVM")
-plt.plot(SAMPLES_PER_CLASS ,STD_FSCORE_SVM[:,0], '--r', linewidth = 2.0, linestyle='solid', color ='r', marker='o',mfc='black', mec='green', ms=10, mew=4, label="SVM (linear)")
+plt.plot(SAMPLES_PER_CLASS ,STD_FSCORE_NEUROCHAOS[:,0], linewidth = 2.0,  linestyle='solid', color ='r', marker='s', ms=10, label="Neurochaos-SVM")
+plt.plot(SAMPLES_PER_CLASS ,STD_FSCORE_SVM[:,0], linewidth = 2.0, linestyle='--', color ='k', marker='o', ms=10, label="SVM (linear)")
+
 
 plt.xticks(SAMPLES_PER_CLASS,fontsize=22)
 plt.yticks(fontsize=22)
 plt.grid(True)
 plt.xlabel('Number of training samples per class', fontsize=22)
 plt.ylabel('Standard deviation of F1-scores', fontsize=22)
-plt.legend(loc="lower right", fontsize=22)
-plt.savefig(RESULT_PATH+ "/standard_deviation_low_training_sample_regime_multi_class_classification.jpg", format='jpg', dpi=200)
+plt.legend(loc="upper right", fontsize=22)
+plt.savefig(RESULT_PATH+ "/standard_deviation_low_training_sample_regime_multi_class_classification.jpg", format='jpg', dpi=300)
 plt.show()
